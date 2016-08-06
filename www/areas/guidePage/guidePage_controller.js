676e34ee9240f3a2ffa1c6c82d821f2958be66a3
whjway <whjway@126.com>
2016-08-06 20:32:06 +0800

    swiper

--- www/areas/guidePage/guidePage_controller.js
+++ www/areas/guidePage/guidePage_controller.js
@@ -1,5 +1,20 @@
 // 引导页路由
 angular.module('guidePage.controller', ['guidePage.service'])
   .controller('GuidePageCtrl',function ($scope) {
-
+  var mySwiper = new Swiper ('.swiper-container', {
+    // direction: 'vertical',
+    // loop: true,
+     onSlideChangeEnd: function(swiper){
+       
+      },
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    
+    // 如果需要前进后退按钮
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    
+    // 如果需要滚动条
+    // scrollbar: '.swiper-scrollbar',
+  }) 
   })
